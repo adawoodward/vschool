abe957ba62e13712a9f913f726c0d33b6e461ef5
Ada Woodward <wonwoodward@gmail.com>
2022-07-15 14:59:04 -0600

    fixed issue of creating too many save buttons

--- moms-shopping-list-start/index.js
+++ moms-shopping-list-start/index.js
@@ -7,11 +7,7 @@ document.getElementById("title").setAttribute("type", "text");
 title.setAttribute('type', 'text');
 
 const input = document.getElementById("title")
-// var x = document.getElementById("title").value;
 
-// var submitBtn = document.querySelector("button");
-// submitBtn.id = "submit";
-// var onclick = submitBtn.getAttribute("onclick");
 var form = document.addItem
 
 form.addEventListener('submit', (e) =>
@@ -24,62 +20,35 @@ form.addEventListener('submit', (e) =>
     input.value = "";
     var appendedItem = list.appendChild(li);
    
-    li.innerHTML += "<div>" + x + "</div>"
-    
-    li.innerHTML += "<button class='editBtn' id='edit' type='button'>" + "edit" + "</button>" + " ";
-    
-    li.innerHTML += "<button class='removeBtn' id='remove' type='button'>" + "X" + "</button>"
+    let div = document.createElement("div");
+    div.textContent = x;
+    let editBtn = document.createElement("button");
+    editBtn.textContent = "edit";
+    let removeBtn = document.createElement("button");
+    removeBtn.textContent = "X";
+    li.append(div, editBtn, removeBtn);
 
-    var remove = document.querySelectorAll('.removeBtn')
-
-    remove.forEach(removeBtn => {
-        removeBtn.addEventListener('click', function() {
-            var li = this.parentNode;
-            li.remove()
-        })
+    removeBtn.addEventListener('click', function() {
+        var li = this.parentNode;
+        li.remove()
     })
 
-    var edit = document.querySelectorAll('.editBtn');
-    var save = document.querySelectorAll('.saveBtn');
-
-    edit.forEach (editBtn => {
-        editBtn.addEventListener('click', function() {
-            var div = this.previousSibling;
-            div.contentEditable = true;
-            // editBtn.innerHTML = "save";
-            editBtn.id = 'save';
-            editBtn.className = 'saveBtn';
-            editBtn.style.display = 'none';
-            var saveBtn = document.createElement("button");
-            saveBtn.textContent = "save";
-            li.appendChild(saveBtn);
-            saveBtn.addEventListener('click', function() {
-                div.contentEditable = false;
-                saveBtn.remove();
-                editBtn.style.display = 'inline-block';
-            })
-            // save.forEach (saveBtn => {
-            //     saveBtn.addEventListener('click', function(e) {
-            //         // var li = this.parentNode;
-            //         // li.removeAttribute("contenteditable");
-            //         saveBtn.innerHTML = "edit";
-            //         saveBtn.id = 'edit';
-            //         // var div = this.previousSibling;
-            //         div.contentEditable = false;
-            //     })
-            // })
+    editBtn.addEventListener('click', function() {
+        var div = this.previousSibling;
+        div.contentEditable = true;
+        // editBtn.innerHTML = "save";
+        editBtn.id = 'save';
+        editBtn.className = 'saveBtn';
+        editBtn.style.display = 'none';
+        var saveBtn = document.createElement("button");
+        saveBtn.textContent = "save";
+        li.appendChild(saveBtn);
+        saveBtn.addEventListener('click', function() {
+            div.contentEditable = false;
+            saveBtn.remove();
+            editBtn.style.display = 'inline-block';
         })
-
     })
 });
 
-
-
-// function createEditBtn() {
-//     const editBtn = document.createElement("button");
-//     editBtn.innerHTML = "edit";
-//     editBtn.id = 'edit';
-//     var onclick = editBtn.getAttribute("onclick");
-//     document.getElementById("li").appendChild(editBtn);
-// }
 
