8914654ec1316488ae9c6a76d4c9d2f8269be490
adawoodward <98771985+adawoodward@users.noreply.github.com>
2022-07-18 15:37:35 -0600

    Add files via upload

---
+++ node-calculator/calculator.js
@@ -0,0 +1,29 @@
+ var readline = require('readline-sync');
+ var userName = readline.question('May I have your name? ');
+ console.log('Hi ' + userName + '!');
+
+ // - A function that adds two numbers and returns the result
+// - A function that multiplies two numbers and returns the result
+// - A function that divides two numbers and returns the result
+// - A function that subtracts two numbers and returns the result
+// - Each function will have 2 parameters, `num1`, `num2`
+// - On the console the prompt will print to the user:
+// - *Please enter your first number* (store that number)
+// - *Please enter your second number* (store that number)
+// - *Please enter the operation to perform: **add**, **sub**, **mul**, **div*** (then store the operation)
+// - Based on the operation entered by the user, you will call one of your 4 functions to perform the correct operation
+// - You will then print to the console: *The result is: [the result]*
+
+var firstNumber = readline.question('Please enter your first number: ');
+var secondNumber = readline.question('Please enter your second number: ');
+// var enteredOperator = readline.question('Please enter the operation to perform: add, sub, mul, div. ');
+const enteredOperator = readline.keyIn("do you want to (a)dd, (s)ubract, (d)ivide, or (m)ultiply? ")
+if (enteredOperator == "a"){
+    console.log("The result is: " + (parseInt(firstNumber)+parseInt(secondNumber)))
+} else if (enteredOperator == "s"){
+    console.log("The result is: " + (firstNumber-secondNumber))
+} else if (enteredOperator == "m"){
+    console.log("The result is: " + (firstNumber*secondNumber))
+} else if (enteredOperator == "d"){
+    console.log("The result is: " + (firstNumber/secondNumber))
+};
