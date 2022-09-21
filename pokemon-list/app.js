0dc83c22ad7d6757923411a1ba85405f7e86f7a2
Ada Woodward <wonwoodward@gmail.com>
2022-09-21 16:01:50 -0600

    commit

---
+++ pokemon-list/app.js
@@ -0,0 +1,29 @@
+const xhr = new XMLHttpRequest()
+xhr.open("GET", "https://api.vschool.io/pokemon", true)
+xhr.send()
+
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        console.log(xhr.responseText)
+        const JSONdata = xhr.responseText
+        const data = JSON.parse(JSONdata)
+        showData(data.objects[0])
+    }
+}
+
+function showData(arr) {
+
+    // Only 8 rows showing
+    // for (let i = 0; i < arr.pokemon[i].name.length; i++) {
+    //     const p = document.createElement("p")
+    //     p.textContent = arr.pokemon[i].name
+    //     document.body.appendChild(p)
+    // }
+
+    // Showing all the names
+    for (let i = 0; i < arr.pokemon.length; i++) {
+        const p = document.createElement("p")
+        p.textContent = arr.pokemon[i].name
+        document.body.appendChild(p)
+    }
+}
