f7349d96bc1ed43620109ae0f3fa96fd6dd091cc
Ada Woodward <wonwoodward@gmail.com>
2022-10-20 13:44:27 -0600

    commit main

---
+++ receiving-props-in-a-component/Contact.js
@@ -0,0 +1,32 @@
+import React from "react"
+
+export default function Contact(props) {
+    /**
+     * Challenge: Fix the code below to use the `props`
+     * object values in place of the hardcoded values below
+     */
+    return (
+        <div className="contact-card">
+            <img src={props.img}/>
+            <h3>{props.name}</h3>
+            <div className="info-group">
+                <img src="./images/phone-icon.png" />
+                <p>{props.phone}</p>
+            </div>
+            <div className="info-group">
+                <img src="./images/mail-icon.png" />
+                <p>{props.email}</p>
+            </div>
+        </div>
+    )
+}
+
+/* 
+{
+    img: "./images/mr-whiskerson.png", 
+    name: "Mr. Whiskerson", 
+    phone: "(212) 555-1234", 
+    email: "mr.whiskaz@catnap.meow"
+}
+
+*/
