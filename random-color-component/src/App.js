52300103c2cfb82f4b536827a8941e9decd2e9b7
Ada Woodward <wonwoodward@gmail.com>
2023-01-09 15:18:49 -0700

    commit

--- random-color-component/src/App.js
+++ random-color-component/src/App.js
@@ -10,20 +10,28 @@ export default function App() {
       .then(data => setColorData(data))
   }, [color])
 
-  console.log(colorData)
+  console.log(colorData.new_color)
 
-  // const backgroundColor = {
-  //   backgroundColor: `#${colorData}`
+  // const myStyle = {
+  //   // backgroundColor: `"#${colorData[0]}"`
+  //   color: `"#${colorData}"`
   // }
 
-  function colorHandleChange() {
-    setColor(prevState => !prevState)
-  }
+  // function colorHandleChange() {
+  //   setColor(prevColor => !prevColor)
+  // }
+
+  // function colorHandleChange() {
+  //   setColor(prevColor => prevColor + 1)
+  // }
 
   return (
-    <div onClick={colorHandleChange} style={{backgroundColor: `#${colorData}`}}>
-      <h2>The color is {colorData}</h2>
-      {/* <button onClick={() => setColor(prevColor => prevColor + 1)}>Get Next Color</button> */}
+    // <div onClick={colorHandleChange} style={{backgroundColor: `#${colorData}`}}>
+    // <div onClick={colorHandleChange} style={{backgroundColor: ["#" + colorData.new_color]}}>
+    // <div onClick={colorHandleChange} style={{backgroundColor: "#" + `${colorData.new_color}`}} >
+    <div style={{backgroundColor: "#" + `${colorData.new_color}`}} >  
+      <h2>The color is #{colorData.new_color}</h2>
+      <button onClick={() => setColor(prevColor => prevColor + 1)}>Get Next Color</button>
       <pre>{JSON.stringify(colorData, null, 2)}</pre>
     </div>
   )
