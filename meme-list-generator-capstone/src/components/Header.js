89b3e96d55148ac40f58194e83b2ec4ef4b09926
Ada Woodward <wonwoodward@gmail.com>
2023-01-31 12:26:19 -0700

    commit

---
+++ meme-list-generator-capstone/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="" className="header--image" />
+            <h2 className="header--title">Meme List Generator</h2>
+            <h4 className="header--project">Capstone</h4>
+        </header>
+    )
+}
---
+++ meme-list-generator-capstone/src/components/Meme.js
@@ -0,0 +1,137 @@
+//- A user will be able to add created memes to a list
+//- A user will be able to delete memes from the list
+//- A user will be able to edit an existing meme
+
+import React from "react"
+
+export default function Meme() {
+    const [userInput, setUserInput] = React.useState({
+        topText: "",
+        bottomText: "",
+        // image: ""
+        image: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [randomImg, setRandomImg] = React.useState("")
+    const [allMemes, setAllMemes] = React.useState([])
+    const [generatedMemes, setGeneratedMemes] = React.useState([])
+
+    React.useEffect(() => {
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMemes()
+    }, [])
+
+    function fetchRandomMeme() {
+        const randomNum = Math.floor(Math.random() * allMemes.length)
+        const randomUrl = allMemes[randomNum].url
+        setRandomImg(randomUrl)
+
+        console.log(randomImg)
+        console.log(randomUrl)
+        // setRandomImg(randomUrl)
+        // console.log(randomUrl)
+
+        // setMeme(prevMeme => ({
+        //     ...prevMeme,
+        //     randomImage: url
+        // }))
+    }
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setUserInput(prevUserInput => ({
+            ...prevUserInput,
+            [name]: value
+        }))
+    }
+
+    // function handleSubmit(event) {
+    //     event.preventDefault()
+    //     setGeneratedMemes((prevGeneratedMemes) => {
+    //         ...prevGeneratedMemes,
+
+    //         topText: meme.topText,
+    //         bottomText: meme.bottomText,
+    //         image: meme.image
+    //     })
+    // }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        setGeneratedMemes(prevGeneratedMemes=> {
+            return [
+                ...prevGeneratedMemes,
+                <div>
+                    <h2>{userInput.topText}</h2>
+                    <h2>{userInput.bottomText}</h2>
+                    <img src={randomImg} />
+                    {/* <img src={userInput.image}></img> */}
+                </div>
+            ]
+            // topText: userInput.topText,
+            // bottomText: userInput.bottomText,
+            // image: userInput.image
+        })
+        // setList(prevList => [...prevList, {meme}])
+    }
+
+    console.log(generatedMemes)
+
+    // const userMeme = generatedMemes.map(meme => 
+    //     <div>
+    //         <h2>{generatedMemes.topText}</h2>
+    //         <h2>{generatedMemes.bottomText}</h2>
+    //         <img src={generatedMemes.image}></img>
+    //     </div>
+    // )
+    
+
+    // console.log(userMeme)
+
+
+    return (
+        <main>
+            <form className="form" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                    name="topText"
+                    value={generatedMemes.topText}
+                    onChange={handleChange}
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                    name="bottomText"
+                    value={generatedMemes.bottomText}
+                    onChange={handleChange}
+                />
+                <button>Refresh</button>
+                <button 
+                    className="form--button"
+                    type="submit"
+                    onClick={fetchRandomMeme}
+                >Submit
+                </button>
+                <img src={randomImg} />
+                {/* <img src={generatedMemes.image} /> */}
+            </form>
+            <h2>List of Memes</h2>
+            <div>{generatedMemes}</div>
+            {/* <div>{userMeme}</div> */}
+            {/* <div className="meme">
+                    <img src={meme.randomImage} className="memeImg" />
+                    <h2 className="meme--top">{meme.topText}</h2>
+                    <h2 className="meme--bottom">{meme.bottomText}</h2>
+            </div> */}
+        </main>
+    )
+
+
+}
