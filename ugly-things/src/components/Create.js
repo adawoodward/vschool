d106a46cfda374bc9eee957690cb0c7a7953713f
Ada Woodward <wonwoodward@gmail.com>
2023-02-27 16:55:08 -0700

    posting works

--- ugly-things/src/components/Create.js
+++ ugly-things/src/components/Create.js
@@ -1,21 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-const Create = () => (
-    <form>
-        <div>
-            <label>Title</label>
-            <input placeholder='Title' />
-        </div>
-        <div>
-            <label>ImgURL</label>
-            <input placeholder='Img URL' />
-        </div>
-        <div>
-            <label>Description</label>
-            <input placeholder='Description' />
-        </div>
-        <button type='submit'>Submit</button>
-    </form>
-)
+export default function Create() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [imgUrl, setImgUrl] = useState('');
+    const postData = () => {
+        axios.post(`https://api.vschool.io/ada/thing/`, {
+            title,
+            description,
+            imgUrl
+        })
+        console.log(title)
+        console.log(description)
+        console.log(imgUrl)
+    }
 
-export default Create
+    return (
+        <div>
+            <form className="create-form">
+                <input 
+                    type='text'
+                    className="form--input"
+                    placeholder='title' 
+                    name='title'
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+
+                <input 
+                    type='text'
+                    className='form--input'
+                    placeholder='description'
+                    name='description' 
+                    onChange={(e) => setDescription(e.target.value)}
+                />
+
+                <input
+                    type='text'
+                    placeholder='imgUrl' 
+                    name='imgUrl'
+                    onChange={(e) => setImgUrl(e.target.value)}
+                />
+
+                <button type='submit' onClick={postData}>Submit</button>
+            </form>
+            <div></div>
+        </div>
+    )
+}
+
+
+// export default Create
