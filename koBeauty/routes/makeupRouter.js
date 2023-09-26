0d43176a558de614197f1ba578dd841ff405378f
Ada Woodward <wonwoodward@gmail.com>
2023-09-26 12:55:43 -0600

    commit

--- koBeauty/routes/makeupRouter.js
+++ koBeauty/routes/makeupRouter.js
@@ -3,12 +3,6 @@ const makeupRouter = express.Router()
 const Makeup = require('../models/makeup')
 const ObjectId = require('mongodb').ObjectId;
 
-// const id = new ObjectId(objectIdString);
-// const ObjectId = require('mongodb').ObjectId
-// const { ObjectId } = require('mongodb')
-// const id = new ObjectId()
-// console.log(id)
-
 makeupRouter.get("/", (req, res, next) => {
     Makeup.find()
     .then((makeups) => res.status(200).send(makeups))
@@ -43,35 +37,6 @@ makeupRouter.get("/", (req, res, next) => {
         // const { id } = req.params
         // await Makeup.findById({_id: id})
     })
-
-// makeupRouter.get("/makeup/:id", (req, res, next) => {
-//     // Makeup.findById(req.params.id)
-//     // console.log(req.params)
-//     // const id = req.params.id
-//     // console.log(id)
-//     // console.log(ObjectId)
-
-//     // const query = { _id: ObjectId(_id) }
-//     // console.log(query)
-
-//     Makeup.findById(req.params.id)
-//     .then((makeups) => res.status(200).send(makeups))
-//     .catch((err) => {
-//         res.status(500)
-//         return next(err)
-//     })
-// })
-
-
-
-// makeupRouter.get("/makeup", (req, res, next) => {
-//     Makeup.findById()
-//     .then((makeups) => res.status(200).send(makeups))
-//     .catch((err) => {
-//         res.status(500)
-//         return next(err)
-//     })
-// })
 
 makeupRouter.get("/search/brand", (req, res, next) => {
     Makeup.find({brand: req.query.brand})
@@ -123,16 +88,5 @@ makeupRouter.put("/:id", (req, res, next) => {
     })
 })
 
-// makeupRouter.get("/:randomId", (req, res, next) => {
-//     const randomId = req.params.randomId
-//     const foundRandom = Makeup.find(random => random._id = randomId)
-//     console.log(foundRandom)
-//     if (!foundRandom) {
-//     const error = new Error(`The item with id ${randomId} was not found!`)
-//     res.status(500)
-//     return next(error)
-//     }
-//     res.status(200).send(foundRandom)
-// })
 
 module.exports = makeupRouter
