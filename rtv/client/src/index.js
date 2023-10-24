fde760421101b6d20eeed4d6b4674d6ed85a5514
Ada Woodward <wonwoodward@gmail.com>
2023-10-24 11:56:32 -0600

    commit

--- rtv/client/src/index.js
+++ rtv/client/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'
+import UserProvider from './context/UserProvider'
+import './css/styles.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <UserProvider>
+        <App />
+      </UserProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
