86a522f77c5c6fe6fb4d889bb142a99036754d61
Ada Woodward <wonwoodward@gmail.com>
2023-11-15 11:54:25 -0700

    commit

--- koBeauty/client/src/component/PostDetail.js
+++ koBeauty/client/src/component/PostDetail.js
@@ -1,26 +1,28 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { UserContext } from '../context/UserProvider';
-import CommentForm from './CommentForm';
+import ReviewForm from './ReviewForm';
 import EditForm from './EditForm';
 
-const IssueDetail = () => {
-    const { userAxios, comments, setComments, postNewComment, setUserState } = useContext(UserContext);
+const PostDetail = () => {
+    const { userAxios, reviews, setReviews, postNewReview, setUserState } = useContext(UserContext);
     const { _id } = useParams();
-    const [issueDetail, setIssueDetail] = useState({});
+    const [postDetail, setPostDetail] = useState({});
     const navigate = useNavigate()
     const [isEditing, setIsEditing] = useState(false)
     const [formData, setFormData] = useState({
         title: '',
+        brand: '',
+        category: '',
         description: '',
         imgUrl: ''
     }); // Add formData state
 
-    const fetchIssue = async () => {
+    const fetchPost = async () => {
         try {
-            const res = await userAxios.get(`/api/issue/issues/${_id}`); 
+            const res = await userAxios.get(`/api/post/posts/${_id}`); 
             console.log(res.data);
-            setIssueDetail(res.data); // updates issueDetail state with the retrieved data
+            setPostDetail(res.data); // updates issueDetail state with the retrieved data
             setFormData(res.data) // sets formData state with the same data retrieved 
             return res.data;
         } catch (err) {
@@ -29,14 +31,14 @@ const IssueDetail = () => {
         }
     };
 
-    const fetchComments = async (issueId) => {
+    const fetchReviews= async (issueId) => {
         try {
-            const res = await userAxios.get(`/api/comment/issues/${issueId}`);
+            const res = await userAxios.get(`/api/review/posts/${postId}`);
             console.log(res.data);
-            setComments(res.data); // updated comments state with res.data, and it will update the comments on frontend
-            console.log(comments);
+            setReviews(res.data); // updated comments state with res.data, and it will update the comments on frontend
+            console.log(reviews);
         } catch (err) {
-            console.error("Error fetching comments: ", err.response);
+            console.error("Error fetching reviews: ", err.response);
             throw err;
         }
     };
@@ -44,64 +46,63 @@ const IssueDetail = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const issueData = await fetchIssue();
-                if (issueData._id) { // if issueData._id exists, it calls fecthComments to fetch comments related to the issue with the same _id
-                    await fetchComments(issueData._id); 
+                const postData = await fetchPost();
+                if (postData._id) { // if issueData._id exists, it calls fecthComments to fetch comments related to the issue with the same _id
+                    await fetchReviews(postData._id); 
                 } else {
-                    console.log("Invalid issueId");
+                    console.log("Invalid postId");
                 }
             } catch (error) {
-                console.error("Error fetching issue: ", error);
+                console.error("Error fetching post: ", error);
             }
         };
-    
         fetchData();
     }, []);
 
     // function to update the comments 
-    const updateComments = async () => {
-        if (issueDetail._id) {
-            await fetchComments(issueDetail._id);  // If the issueDetail has a valid _id, it calls the fetchComments function, passing the _id as an argument to retrieve comments associated with that specific issue from the server
+    const updateReviews = async () => {
+        if (postDetail._id) {
+            await fetchReviews(postDetail._id);  // If the issueDetail has a valid _id, it calls the fetchComments function, passing the _id as an argument to retrieve comments associated with that specific issue from the server
         } // If there's no valid _id, the function won't make the call to fetch comments
     };
 
-    async function postComment(newComment) {
-        if (!issueDetail._id) {
-            console.error("Invalid issueId");
+    async function postReview(newReview) {
+        if (!postDetail._id) {
+            console.error("Invalid postId");
             return;
         }
-        console.log("issueId: ", issueDetail._id);
+        console.log("postId: ", postDetail._id);
     
         try {
-            await postNewComment(newComment, issueDetail._id); // call postNewComment to add the new comment to the issue with issueDetail._id
-            await updateComments(); // after posting comment, it calls updateComments function to fetch and update the comments
-            console.log(comments);
+            await postNewReview(newReview, postDetail._id); // call postNewComment to add the new comment to the issue with issueDetail._id
+            await updateReviews(); // after posting comment, it calls updateComments function to fetch and update the comments
+            console.log(reviews);
         } catch (error) {
-            console.error("Error updating comments: ", error);
+            console.error("Error updating reviews: ", error);
         }
     }
 
-    async function deleteIssue() {
+    async function deletePost() {
         try {
-            const url = `/api/issue/${_id}`;
-            console.log("Deleting issue at URL:", url);
+            const url = `/api/post/${_id}`;
+            console.log("Deleting post at URL:", url);
             await userAxios.delete(url);
             setUserState(prevUserState => ({
                 ...prevUserState,
-                issues: prevUserState.issues.filter(issue => issue._id !== _id)
+                posts: prevUserState.posts.filter(post => post._id !== _id)
             })); // updating userState by removing the deleted issue from issues array
             navigate('/profile') 
         } catch (error) {
-            console.error('Error deleting issue:', error);
+            console.error('Error deleting post:', error);
         }
     }
     
-    async function deleteComment(commentId) {
+    async function deleteReview(reviewId) {
         try {
-            await userAxios.delete(`/api/comment/${commentId}`)
-            await updateComments() // to refresh the comments after deleting
+            await userAxios.delete(`/api/review/${reviewId}`)
+            await updateReviews() // to refresh the comments after deleting
         } catch (error) {
-            console.error('Error deleting comment:', error)
+            console.error('Error deleting review:', error)
         }
     }
 
@@ -109,22 +110,22 @@ const IssueDetail = () => {
     const handleSave = async (updatedFormData) => { // takes updatedFormData as an argument
         try {
         // Make API request to update the issue with updatedFormData
-        await userAxios.put(`/api/issue/${_id}`, updatedFormData);
+        await userAxios.put(`/api/post/${_id}`, updatedFormData);
       
         // After successful update, set isEditing to false
         setIsEditing(false);
 
         // Refetch the issue and comments to update the UI
-        await fetchIssue();
-        await updateComments();
+        await fetchPost();
+        await updateReviews();
 
         // Add the following line to trigger a re-render in the Profile component
         setUserState(prevUserState => ({
             ...prevUserState,
-            issues: prevUserState.issues.map(issue => (issue._id !== _id ? issue : { ...issue, ...updatedFormData })),
+            posts: prevUserState.posts.map(post => (post._id !== _id ? post : { ...post, ...updatedFormData })),
         })); // maps through prevUserState.issues array and looks for the specific issue with the same _id, replaces old data with updatedFormData
         } catch (error) {
-        console.error('Error updating issue:', error);
+        console.error('Error updating post:', error);
         }
     };
       
@@ -145,33 +146,37 @@ const IssueDetail = () => {
                 onSave={handleSave}
                 />) : (
                 <div className='detail'>
-                <div>Title: {issueDetail?.title}</div>
+                <div>Title: {postDetail?.title}</div>
+                <br></br>                
+                <div>Brand: {postDetail?.brand}</div>
                 <br></br>
-                <div>Description: {issueDetail?.description}</div>
+                <div>Category: {postDetail?.category}</div>
                 <br></br>
-                <div>ImgUrl: {issueDetail?.imgUrl}</div>
+                <div>Description: {postDetail?.description}</div>
+                <br></br>
+                <div>ImgUrl: {postDetail?.imgUrl}</div>
                 <br></br>
                 </div>
                 )}
                 <div className='edit-delete-buttons'>
-                {isEditing ? (<div></div>) : (<button className='edit' onClick={() => setIsEditing(true)}>Edit Issue</button>)}
-                <button className='delete' onClick={deleteIssue}>Delete Issue</button>
+                {isEditing ? (<div></div>) : (<button className='edit' onClick={() => setIsEditing(true)}>Edit Post</button>)}
+                <button className='delete' onClick={deleteIssue}>Delete Post</button>
                 </div>
                 <br></br>
                 {/* {isEditing ? (<div></div>) : (<button className='edit' onClick={() => setIsEditing(true)}>Edit Issue</button>)}
                 <button className='delete' onClick={deleteIssue}>Delete Issue</button> */}
-                <div className='comment-container'>Comment
-                {issueDetail._id ? (
-                    <CommentForm postComment={postComment} issueId={issueDetail._id} />
+                <div className='review-container'>REVIEW
+                {postDetail._id ? (
+                    <ReviewForm postReview={postReview} postId={postDetail._id} />
                 ) : (
                     <p>Loading...</p>
                 )}
                 <br></br>
-                <div className='comment'>
-                {comments?.map((comment) => (
-                    <div key={comment._id}>
-                        {comment.text}
-                        <button onClick={() => deleteComment(comment._id)}>Delete Comment</button>
+                <div className='review'>
+                {reviews?.map((review) => (
+                    <div key={review._id}>
+                        {review.text}
+                        <button onClick={() => deleteReview(review._id)}>Delete Review</button>
                     </div>
                 ))}
                 </div>
@@ -183,5 +188,5 @@ const IssueDetail = () => {
     );
 }
 
-export default IssueDetail;
+export default PostDetail;
 
---
+++ koBeauty/client/src/component/ReviewForm.js
@@ -0,0 +1,90 @@
+import React, { useContext, useState } from 'react'
+import { UserContext } from '../context/UserProvider'
+
+
+const initInputs = {
+    text: "",
+    imgUrl: "",
+    rating: 1 // Default rating value
+}
+
+export default function ReviewForm({ postId, postReview }) {
+    // const [text, setText] = useState('')
+    const [inputs, setInputs] = useState(initInputs)
+    const { user } = useContext(UserContext)
+
+    // const handleInputChange = (e) => {
+    //     setText(e.target.value)
+    // }
+
+    function handleInputChange(e){
+        const {name, value} = e.target
+        setInputs(prevInputs => ({
+          ...prevInputs,
+          [name]: value
+        }))
+      }
+
+      function handleRatingChange(e) {
+        const { value } = e.target;
+        setInputs((prevInputs) => ({
+          ...prevInputs,
+          rating: parseInt(value), // Convert to integer
+        }));
+      }
+    
+      const handleReviewSubmit = (e) => {
+        e.preventDefault();
+        const { text, imgUrl, rating } = inputs; // Destructure inputs
+        if (text && user) {
+          postReview({ text, imgUrl, post: postId, rating }); // Include rating in the postReview function
+          setInputs(initInputs);
+        }
+      };
+
+    // const handleReviewSubmit = (e) => {
+    //     e.preventDefault()
+    //     if (text && user) {
+    //         postReview({ text, imgUrl, post: postId })
+    //         setInputs(initInputs)
+    //     }
+    // }
+
+
+
+    return (
+      <div className='review-form'>
+          <h3>Add a Review</h3>
+          {user ? (
+              <form onSubmit={handleReviewSubmit}>
+                  <textarea
+                      name="text"
+                      value={text}
+                      onChange={handleInputChange}
+                      placeholder="Write your comment here"
+                  />
+                  <input 
+                    type="text" 
+                    name="imgUrl" 
+                    value={imgUrl} 
+                    onChange={handleInputChange} 
+                    placeholder="ImageUrl here"/>
+                  <br></br>
+                  <br />
+                  <label htmlFor="rating">Rating:</label>
+                  <select name="rating" value={inputs.rating} onChange={handleRatingChange}>
+                  {[1, 2, 3, 4, 5].map((value) => (
+                    <option key={value} value={value}>
+                        {value}
+                    </option>
+                  ))}
+                  </select>
+                  <br></br>
+                  <button type="submit">Submit Review</button>
+              </form>
+          ) : (
+              <p>Please sign in to post a review.</p>
+          )}
+      </div>
+  );
+}
