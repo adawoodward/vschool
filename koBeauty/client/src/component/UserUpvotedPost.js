0153ce7564814ea8ab32a96da7c421f61f33d4d9
Ada Woodward <wonwoodward@gmail.com>
2023-11-20 10:41:01 -0700

    commit

--- koBeauty/client/src/component/UserUpvotedPost.js
+++ koBeauty/client/src/component/UserUpvotedPost.js
@@ -1,13 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { UserContext } from '../context/UserProvider';
 import Post from './Post';
 
 export default function UserUpvotedPosts() {
-  const { user, allPosts } = useContext(UserContext);
+  const { user, allPosts, getAllPosts } = useContext(UserContext);
+
+  console.log(allPosts)
+  useEffect(() => {
+    // Fetch all issues from the server
+    getAllPosts()
+  }, []);
 
   // Filter the posts the user has upvoted
-//   const userUpvotedPosts = allPosts.filter(post => post.likedUsers && post.likedUsers.includes(user._id));
-const userUpvotedPosts = allPosts.filter(post => post.likedUsers && post.likedUsers.some(id => id === user._id));
+  // const userUpvotedPosts = allPosts.filter(post => post.likedUsers && post.likedUsers.includes(user._id));
+  const userUpvotedPosts = allPosts.filter(post => post.likedUsers && post.likedUsers.some(id => id === user._id));
 
   console.log(userUpvotedPosts)
 
